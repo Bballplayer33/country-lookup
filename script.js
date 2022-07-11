38a9a723d3b3d3d0809cfc53379cb80d9ce344b4
Earl Adams <earladams2000@gmail.com>
2022-07-11 15:39:25 -0400

    bug fixing api calls

---
+++ script.js
@@ -0,0 +1,8 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInput = document.getElementById("country-input");
+
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInput.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+});
