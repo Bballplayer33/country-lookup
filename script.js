dc617e67db66b31407154614d5333c492510d020
Earl Adams <earladams2000@gmail.com>
2022-07-11 16:39:39 -0400

    api's setup complete

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ searchBtn.addEventListener("click", () => {
         console.log(data[0].flags.svg);
         console.log(data[0].name.common);
         console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies));
+        console.log(Object.keys(data[0].currencies)[0]);
         console.log(data[0].currencies[Object.keys(data[0].currencies)].name); 
-        console.log(Object.values(data[0].langueges.toString().split(",").join(",")));
-    });
+        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
 
-    result.innerHTML = `
+        result.innerHTML = `
         <img src= "${data[0].flags.svg}"
-    `
+        class = "flag-img">
+    `;
+        
+    });
 
 });
